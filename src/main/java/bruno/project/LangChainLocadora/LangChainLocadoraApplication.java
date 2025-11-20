c0883e301ad05f280d314662f15ece75628f7a5b
Bruno Grassi <brunoograssi@hotmail.com>
2025-11-20 19:46:53 -0300

    Create assistant AI Locadora project

---
+++ src/main/java/bruno/project/LangChainLocadora/LangChainLocadoraApplication.java
@@ -0,0 +1,21 @@
+package bruno.project.LangChainLocadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangChainLocadoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangChainLocadoraApplication.class, args);
+    }
+//	Client client = new Client();
+//
+//	GenerateContentResponse response =
+//			client.models.generateContent(
+//					"gemini-2.5-flash",
+//					"Explain how AI works in a few words",
+//					null);
+//
+//    System.out.println(response.text());
+}
---
+++ src/test/java/bruno/project/LangChainLocadora/LangChainLocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package bruno.project.LangChainLocadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangChainLocadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
